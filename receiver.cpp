cbdb9f73c0eeb1ec0ec41c92ce82ecbfa915381e
Yoohyuk Chang <yoohyukchang@gmail.com>
2023-11-29 09:49:11 -0500

    updated sender.cpp

--- receiver.cpp
+++ receiver.cpp
@@ -21,13 +21,103 @@ int main(int argc, char **argv) {
   Connection conn;
 
   // TODO: connect to server
+  // Connect to server
+  conn.connect(server_hostname, server_port);
+  // If connection is not open, then it means we have error with connecting to server
+  if (!conn.is_open()) {
+    std::cerr << "Couldn't connect to server.\n";
+    exit(1);
+  }
 
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
 
+  // send rlogin
+  Message rlogin_msg(TAG_RLOGIN, username);
+  bool send_status = conn.send(rlogin_msg);
+  if (!send_status) {
+    std::cerr << "Send rlogin not successful.\n";
+    exit(1);
+  }
+
+  // check response from the server
+  Message rlogin_response;
+  bool receive_status = conn.receive(rlogin_response);
+  if (!receive_status) {
+    std::cerr << "Receive from server not successful.\n";
+    exit(1);
+  }
+  if (rlogin_response.tag == TAG_ERR) {
+    std::string err_msg = rlogin_response.data;
+    std::cerr << err_msg.c_str() << "\n";
+    exit(1);
+  }
+
+  // join messages
+  Message join_msg(TAG_JOIN, room_name);
+  send_status = conn.send(join_msg);
+  if (!send_status) {
+    std::cerr << "join messages not successful.\n";
+    exit(1);
+  }
+
+  // check response from the server
+  Message join_response;
+  receive_status = conn.receive(join_response);
+  if (!receive_status) {
+    std::cerr << "Receive from server not successful.\n";
+    exit(1);
+  }
+  if (join_response.tag == TAG_ERR) {
+    std::string err_msg = join_response.data;
+    std::cerr << err_msg.c_str() << "\n";
+    exit(1);
+  }
+
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
+  // We assume that the delivery from the server will be in the format 'delivery:[room]:[sender]:[message]'. (except for error cases)
+  while (1) {
+    Message msg_from_server;
+    receive_status = conn.receive(msg_from_server);
+    if (!receive_status) {
+      std::cerr << "Receive from server not successful.\n";
+      exit(1);
+    }
+    if (msg_from_server.tag == TAG_ERR) {
+      std::string err_msg = msg_from_server.data;
+      std::cerr << err_msg.c_str() << "\n";
+      exit(1);
+    }
+    if (msg_from_server.tag == TAG_DELIVERY) {
+      // print to stdout with [username of sender]: [message text]
+      std::string received_data = msg_from_server.data;
 
+      int data_length = received_data.size(); // length of the data received
+      int prev_colon_pos = 0; // pointer to track whenever we find a new colon.
+      std::vector<std::string> data_storage;
+
+      // Iterate through the data to separate the pieces of data by ':'
+      for (int i = 0; i < data_length; i++) {
+        char curr_char = received_data[i];
+        if (curr_char == ':') {
+          std::string data_piece = received_data.substr(prev_colon_pos, i - prev_colon_pos);
+          prev_colon_pos = i + 1;
+          data_storage.push_back(data_piece);
+        }
+      }
+
+      // Handle the last piece of data
+      if (prev_colon_pos < data_length) {
+        data_storage.push_back(received_data.substr(prev_colon_pos));
+      }
+      
+      // Print data_storage[1] and data_storage[2] if they exist
+      if (data_storage.size() > 2) {
+        fprintf(stdout, "%s: %s\n", data_storage[1].c_str(), data_storage[2].c_str());
+      }
+    }
+  }
 
   return 0;
 }
--- sender.cpp
+++ sender.cpp
@@ -25,34 +25,143 @@ int main(int argc, char **argv) {
   Connection conn;
   conn.connect(server_hostname, server_port);
   if (!conn.is_open()) {
-    std::cerr << "Couldn't connect to server.";
-    exit(1);
-  }
-
-
-  Message slogin_msg(TAG_SLOGIN, username);
-  bool send_status = conn.send(slogin_msg);
-  if (!send_status) {
-    std::cerr << "Send rlogin not successful.";
-    exit(1);
-  }
-
-  Message recived_msg;
-  bool recived_status = conn.receive(recived_msg);
-  if (!recived_status) {
-    std::cerr << "Recived rlogin not successful.";
-    exit(1);
-  }
-  if (recived_msg.tag == TAG_ERR) {
-    std::string err_msg = recived_msg.data;
-    std::cerr << err_msg.c_str();
+    std::cerr << "Couldn't connect to server.\n";
     exit(1);
   }
 
   // TODO: send slogin message
+  Message slogin_msg(TAG_SLOGIN, username);
+  bool send_status = conn.send(slogin_msg);
+  if (!send_status) {
+    std::cerr << "Send slogin not successful.\n";
+    exit(1);
+  }
+
+  Message received_msg;
+  bool recived_status = conn.receive(received_msg);
+  if (!recived_status) {
+    std::cerr << "Received slogin not successful.\n";
+    exit(1);
+  }
+  if (received_msg.tag == TAG_ERR) {
+    std::string err_msg = received_msg.data;
+    std::cerr << err_msg.c_str() << "\n";
+    exit(1);
+  }
+
 
   // TODO: loop reading commands from user, sending messages to
   //       server as appropriate
+  while (1) {
+    bool send_status;
+    bool receive_status;
+    std::string user_command;
+    std::getline(std::cin, user_command);
+
+    // join:[room]
+    std::string command_type = "/join ";
+    size_t pos = user_command.find(command_type);
+    // if '/join' is in the user_command
+    if (pos != std::string::npos) {
+      // extract the [room name] which starts right after the command_type with a space
+      std::string room_name = user_command.substr(pos + command_type.length());
+      // create the message with the extracted room name
+      Message msg_to_server(TAG_JOIN, room_name);
+      // check the status of sending to the server
+      send_status = conn.send(msg_to_server);
+      if (!send_status) {
+        std::cerr << "Sending join not successful.\n";
+        exit(1);
+      }
+      Message join_receive_response;
+      receive_status = conn.receive(join_receive_response);
+      if (!receive_status) {
+        std::cerr << "Receiving join not successful.\n";
+        exit(1);
+      }
+      if (join_receive_response.tag == TAG_ERR) {
+        std::string err_msg = join_receive_response.data;
+        std::cerr << err_msg.c_str() << "\n";
+      }
+      continue;
+    }
+
+
+    // sendall:[message text]
+    command_type = "/sendall";
+    pos = user_command.find(command_type);
+    // if '/sendall' is found
+    if (pos != std::string::npos) {
+      Message msg_sendall(TAG_SENDALL, user_command);
+      send_status = conn.send(msg_sendall);
+      if (!send_status) {
+        std::cerr << "Sending sendall not successful.\n";
+        exit(1);
+      }
+      Message sendall_response;
+      receive_status = conn.receive(sendall_response);
+      if (!receive_status) {
+        std::cerr << "Receiving after sendall not successful.\n";
+        exit(1);
+      }
+      if (sendall_response.tag == TAG_ERR) {
+        std::string err_msg = sendall_response.data;
+        std::cerr << err_msg.c_str() << "\n";
+      }
+      continue;
+    }
+
+
+    // leave:
+    command_type = "/leave";
+    pos = user_command.find(command_type);
+    //if '/leave' is found
+    if (pos != std::string::npos) {
+      Message msg_leave(TAG_LEAVE, "");
+      send_status = conn.send(msg_leave);
+      if (!send_status) {
+        std::cerr << "Sending leave not successful.\n";
+        exit(1);
+      }
+      Message leave_response;
+      receive_status = conn.receive(leave_response);
+      if (!receive_status) {
+        std::cerr << "Receiving after leave not successful.\n";
+        exit(1);
+      }
+      if (leave_response.tag == TAG_ERR) {
+        std::string err_msg = leave_response.data;
+        std::cerr << err_msg.c_str() << "\n";
+      }
+      continue;
+    }
+
+
+    // quit:
+    command_type = "/quit";
+    pos = user_command.find(command_type);
+    // if '/quit' is found
+    if (pos != std::string::npos) {
+      Message msg_quit(TAG_QUIT, "");
+      send_status = conn.send(msg_quit);
+      if (!send_status) {
+        std::cerr << "Sending quit not successful.\n";
+        exit(1);
+      }
+      Message quit_response;
+      receive_status = conn.receive(quit_response);
+      if (!receive_status) {
+        std::cerr << "Receiving after quit not successful.\n";
+        exit(1);
+      }
+      if (quit_response.tag == TAG_ERR) {
+        std::string err_msg = quit_response.data;
+        std::cerr << err_msg.c_str() << "\n";
+      }
+      break;
+    }
+
+  }
 
   return 0;
 }
